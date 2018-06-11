40c2f9d37d87bec511a71222ac31b7e60e0a4c5d
Calvin Feng <calvinjhfeng@gmail.com>
2018-06-10 23:26:21 -0700

    Added UI code

---
+++ ui/util.ts
@@ -0,0 +1,20 @@
+/**
+ * @author Calvin Feng
+ */
+
+ import Axios from "axios";
+
+
+ export async function classifyImageFile(file: File): Promise<string> {
+    const formData: FormData = new FormData();
+    formData.append("image", file);
+
+    try {
+        const result = await Axios.post("api/tf/recognition/", formData, {
+            headers: { 'Content-Type': 'multipart/form-data' }
+        });
+        return result.data.message;
+    } catch(e) {
+        throw e;
+    }
+ }
